a3abad8b778e80a0378c2db8b066c84e06140c7c
isad9997 <iarena@ia.com.mx>
2021-10-15 17:31:28 -0500

    Feat: Header Component
    Add to Header Component With Containing Actios for Search and
    Navigations Pages.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-
-import './App.css';
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-      {/*Header*/}
+    <div>
+      <Header />
       {/*Home*/}
-      
     </div>
   );
 }
