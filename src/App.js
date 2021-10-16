47c1c0245e4cf704e41eb2c67ffea65f9ad9e165
isad9997 <iarena@ia.com.mx>
2021-10-15 21:36:10 -0500

    Feat: Home Page Complete
    Home Page Complete and Render Images For Test.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
 import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Header />
-      {/*Home*/}
+      <Router>
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>Pagina de pago</h1>
+          </Route>
+
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
